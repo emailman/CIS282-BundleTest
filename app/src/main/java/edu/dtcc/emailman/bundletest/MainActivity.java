ad56d7f489e16e453288ce6033f3b16b75c324aa
emailman <emailman@dtcc.edu>
2017-01-22 11:41:59 -0500

    Implemented bundle

--- app/src/main/java/edu/dtcc/emailman/bundletest/MainActivity.java
+++ app/src/main/java/edu/dtcc/emailman/bundletest/MainActivity.java
@@ -2,6 +2,7 @@ package edu.dtcc.emailman.bundletest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,26 +10,77 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Define two keys for the bundle
+    static final String AREA_KEY = "area_value";
+    static final String PERIMETER_KEY = " perimeter_value";
+
+    // Define variables for area and perimeter
+    static double area;
+    static double perimeter;
+
+    // Define the input fields for length and width
+    static EditText etLength;
+    static EditText etWidth;
+
+    // Define the output fields for area and perimeter
+    static TextView tvArea;
+    static TextView tvPerimeter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get references to the output fields
+        tvArea = (TextView) findViewById(R.id.tvArea);
+        tvPerimeter = (TextView) findViewById(R.id.tvPerimeter);
+
+        // Get references to the input fields
+        etLength = (EditText) findViewById(R.id.etLength);
+        etWidth = (EditText) findViewById(R.id.etWidth);
+
+        // Is there a saved bundle?
+        if (savedInstanceState != null) {
+            // Restore the saved data
+            area = savedInstanceState.getDouble(AREA_KEY);
+            perimeter = savedInstanceState.getDouble(PERIMETER_KEY);
+
+            tvArea.setText(Double.toString(area));
+            tvPerimeter.setText(Double.toString(perimeter));
+            Log.d("ERIC", "bundle restored");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putDouble(AREA_KEY, area);
+        outState.putDouble(PERIMETER_KEY, perimeter);
+
+        Log.d("ERIC","bundle saved");
     }
 
     public void onCalcClick(View view) {
-        Toast.makeText(this, "Calc Clicked", Toast.LENGTH_LONG).show();
-        EditText etLength = (EditText) findViewById(R.id.etLength);
-        EditText etWidth = (EditText) findViewById(R.id.etWidth);
-        TextView tvArea = (TextView) findViewById(R.id.tvArea);
-        TextView tvPerimeter = (TextView) findViewById(R.id.tvPerimeter);
 
-        double length = 7;
-        double width = 14;
-        double area = length * width;
-        double perimeter = 2 * (length + width);
+        try {
+            // Read the input fields
+            double length = Double.parseDouble(etLength.getText().toString());
+            double width = Double.parseDouble(etWidth.getText().toString());
 
-        tvArea.setText("x");
-        tvPerimeter.setText("y");
+            // Do the calculations
+            area = length * width;
+            perimeter = 2 * (length + width);
 
+            // Put the results in the output fields
+            tvArea.setText(Double.toString(area));
+            tvPerimeter.setText(Double.toString(perimeter));
+        }
+        catch (Exception e ){
+            Toast.makeText(MainActivity.this, "Missing Input", Toast.LENGTH_LONG).show();
+            // Clear the output fields
+            tvArea.setText("");
+            tvPerimeter.setText("");
+        }
     }
 }
